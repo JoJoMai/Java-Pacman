d3cf3a8a5d438e94c99cd1a0cd31acced5711dfd
TrungMai <bieuluu53@gmail.com>
2025-08-10 16:11:43 +0700

    create window

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int columnCount = 19;
+        int rowCount = 21;
+        int titleSize = 32;
+        int boardWith = columnCount * titleSize;
+        int boardHeight = rowCount * titleSize; 
+
+        JFrame frame = new JFrame("Pacman");
+        frame.setVisible(true);
+        frame.setSize(boardWith, boardHeight);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+
+
     }
 }
